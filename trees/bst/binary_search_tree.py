a2f78a81e91996fab02ce65e085094e133120515
Kyle Brown Aisho <kyleaisho@gmail.com>
2023-02-04 05:35:56 -0800

    Finished BST

--- trees/bst/binary_search_tree.py
+++ trees/bst/binary_search_tree.py
@@ -1,3 +1,4 @@
+from typing import Optional
 from ..bst.node import Node
 
 
@@ -11,21 +12,100 @@ def build_bst(items) -> Node:
     return root
 
 
-def insert(root: Node, item) -> Node:
-    if not root:
+def insert(node: Node, item) -> Node:
+    if not node:
         return Node(item)
-    if item <= root.item:
-        if not root.left:
-            root.left = Node(item)
+    if item <= node.item:
+        if not node.left:
+            node.left = Node(item)
+            node.left.parent = node
         else:
-            insert(root.left, item)
+            insert(node.left, item)
     else:
-        if not root.right:
-            root.right = Node(item)
+        if not node.right:
+            node.right = Node(item)
+            node.right.parent = node
         else:
-            insert(root.right, item)
-    return root
+            insert(node.right, item)
+    return node
 
 
-def delete(node: Node, item) -> Node:
-    pass
+def find(node: Node, item) -> Optional[Node]:
+    if not node:
+        return
+    if node.item == item:
+        return node
+    return find(node.left, item) if item <= node.item else find(node.right, item)
+
+
+def get_min(node: Node) -> Optional[Node]:
+    if not node:
+        return None
+    if not node.left:
+        return node
+    return get_min(node.left)
+
+
+def get_max(node: Node) -> Optional[Node]:
+    if not node:
+        return None
+    if not node.right:
+        return node
+    return get_max(node.right)
+
+
+def get_successor(node: Node) -> Optional[Node]:
+    if not node:
+        return
+    if node.right:
+        return get_min(node.right)
+    # If there is no right subtree the successor is the
+    # first left turn in my ancestor tree
+    parent = node.parent
+    current = node
+    while parent and parent.right == current:
+        current = parent
+        parent = parent.parent
+
+    return parent
+
+
+def get_predecessor(node: Node) -> Optional[Node]:
+    if not node:
+        return None
+    if node.left:
+        return get_max(node.left)
+    parent = node.parent
+    while parent and parent.left == node:
+        node = parent
+        parent = parent.parent
+    return parent
+
+
+def is_leaf(node: Node) -> bool:
+    return not (node.left or node.right)
+
+
+def remove_from_parent(node: Node) -> bool:
+    if node.parent.left == node:
+        node.parent.left = None
+    if node.parent.right == node:
+        node.parent.right = None
+    return True
+
+
+def swap_nodes(n1: Node, n2: Node) -> None:
+    tmp = n1.item
+    n1.item = n2.item
+    n2.item = tmp
+
+
+def delete(node: Node) -> bool:
+    if node is None:
+        return False
+    if is_leaf(node):
+        return remove_from_parent(node)
+
+    swap_node = get_predecessor(node) if node.left else get_successor(node)
+    swap_nodes(node, swap_node)
+    return delete(swap_node)
--- trees/bst/test_binary_search_tree.py
+++ trees/bst/test_binary_search_tree.py
@@ -1,4 +1,13 @@
-from ..bst.binary_search_tree import insert, build_bst
+from ..bst.binary_search_tree import (
+    delete,
+    get_max,
+    get_predecessor,
+    get_successor,
+    insert,
+    build_bst,
+    find,
+    get_min,
+)
 
 
 def test_insert():
@@ -27,3 +36,87 @@ def test_build():
     assert root.right.item == 16
     assert root.left.right.item == 5
     assert root.right.right.item == 17
+
+
+def test_find():
+    items = [10, 4, 16, 5, 17, 21, 1]
+    root = build_bst(items)
+
+    assert find(root, 10) == root
+    assert find(root, 4) == root.left
+    assert find(root, 16) == root.right
+    assert find(root, 5) == root.left.right
+
+
+def test_get_min():
+    items = [10, 4, 16, 5, 17, 21, 1]
+    root = build_bst(items)
+
+    assert get_min(root) == root.left.left
+    assert get_min(root.right) == root.right
+    assert get_min(root.left.left) == root.left.left
+    assert get_min(root.right.right) == root.right.right
+
+
+def test_get_max():
+    items = [10, 4, 16, 5, 17, 21, 1]
+    root = build_bst(items)
+
+    assert get_max(root) == root.right.right.right
+    assert get_max(root.left) == root.left.right
+    assert get_max(root.left.left) == root.left.left
+
+
+def test_get_successor():
+    items = [10, 4, 17, 5, 16, 21, 1]
+    root = build_bst(items)
+
+    assert get_successor(None) == None
+    assert get_successor(root) == get_min(root.right)
+    assert get_successor(root.right.right) == None
+    assert get_successor(root.left.left) == root.left
+
+
+def test_get_predecessor():
+    items = [10, 4, 17, 5, 16, 21, 1]
+    root = build_bst(items)
+
+    assert get_predecessor(None) == None
+    assert get_predecessor(root) == get_max(root.left)
+    assert get_predecessor(root.left) == root.left.left
+    assert get_predecessor(root.right.right) == root.right
+
+
+def test_delete():
+    items = [10, 4, 17, 5, 16, 21, 1]
+    root = build_bst(items)
+
+    node = find(root, 1)
+    delete(node)
+    assert root.left.left is None
+    insert(root, 1)
+
+    node = find(root, 4)
+    assert delete(node)
+    assert root.left.item == 1
+    assert root.left.left is None
+    assert root.left.right.item == 5
+
+    node = find(root, 10)
+    delete(node)
+    assert root.item == 5
+    assert find(root, 10) is None
+
+    node = find(root, 17)
+    delete(node)
+    assert root.right.item == 16
+    assert find(root, 17) == None
+
+    insert(root, 19)
+    assert root.right.right.left.item == 19
+
+    node = find(root, 21)
+    delete(node)
+    assert root.right.right.item == 19
+    assert root.right.right.right is None
+    assert root.right.right.left is None
